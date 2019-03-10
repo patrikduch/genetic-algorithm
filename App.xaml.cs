be6001bf51cd00f9d00cd36726a130d7ea5961a6
patrikduch <duchpatrik@icloud.com>
2019-03-10 15:06:42 +0100

    Added WPF template

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="App.cs" website="Patrikduch.com">
+//     Copyright 2019 (c) Patrikduch.com
+// </copyright>
+// <author>Patrik Duch</author>
+//-----------------------------------------------------------------------
+
+namespace Desktop
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainWindow.cs" website="Patrikduch.com">
+//     Copyright 2019 (c) Patrikduch.com
+// </copyright>
+// <author>Patrik Duch</author>
+//-----------------------------------------------------------------------
+
+namespace Desktop
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void SetupWindow()
+        {
+            this.Top = 0;
+            this.Left = 0;
+
+            var tmp = SystemParameters.WorkArea.Width;
+            var to = SystemParameters.WorkArea.Height;
+
+            this.Width = tmp;
+
+            //this.Width = SystemParameters.MaximizedPrimaryScreenWidth- SystemParameters.BorderWidth;
+            //this.Height = SystemParameters.MaximumWindowTrackHeight;
+
+            this.Height = to;
+
+            this.Title = "Genetic algorithm";
+        }
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            SetupWindow();
+
+
+        }
+    }
+}
